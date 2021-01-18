892c823c2756a059f6630688d15ee7a97e2985e5
deaconWong <wangiye@163.com>
2021-01-18 16:34:33 +0800

    删除日志

--- src/main/java/deacon/test/spb/cache/CacheConfiguration.java
+++ src/main/java/deacon/test/spb/cache/CacheConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
